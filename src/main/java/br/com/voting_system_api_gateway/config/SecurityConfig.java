fda861dbcfef712648d255a8e9cd923fa8f9b4f3
SdneyFernandes <fsidney987@gmail.com>
2025-08-29 00:08:00 -0300

    g

---
+++ src/main/java/br/com/voting_system_api_gateway/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable) // CSRF não faz sentido em API Gateway
+            .cors(cors -> {}) // 🔑 Habilita o CORS antes do resto
+            .authorizeExchange(exchange -> exchange
+                .pathMatchers("/api/auth/**").permitAll() // login/registro liberados
+                .anyExchange().authenticated() // o resto exige auth
+            );
+
+        return http.build();
+    }
+}
